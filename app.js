9837421de2e9ab83b553f25fcbae2cd5b95b262f
AlisonEstrada <73503380+AlisonEstrada@users.noreply.github.com>
2021-05-20 11:49:26 -0400

    20/05

--- app.js
+++ app.js
@@ -449,9 +449,9 @@ app.post('/ingresarNuevoAcceso', (req, res) => {
                     res.writeHead(200, {'Content-Type': 'application/json'});
                     res.end(JSON.stringify(rows));
                     } 
-                    /*else {
-                    res.redirect('/pacienteInicio');
-                    }*/
+                    else {
+                    res.redirect('/MedicoListPac');
+                    }
                    closeDb();
                })
 })
